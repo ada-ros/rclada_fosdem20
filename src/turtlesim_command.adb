0fe1b7e15754681aca1d86254e30e4056dc7d81c
Alejandro R. Mosteo <amosteo@unizar.es>
2020-01-26 23:28:48 +0100

    It's alive!

--- src/turtlesim_command.adb
+++ src/turtlesim_command.adb
@@ -1,3 +1,4 @@
+with Ada.Calendar;     use Ada.Calendar;
 with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Text_IO;      use Ada.Text_IO;
 
@@ -16,7 +17,8 @@ procedure Turtlesim_Command is
    Support : constant ROSIDL.Typesupport.Message_Support :=
                ROSIDL.Typesupport.Get_Message_Support
                  ("geometry_msgs", "Twist");
-   Msg : ROSIDL.Dynamic.Message := ROSIDL.Dynamic.Init (Support);
+   MsgLin  : ROSIDL.Dynamic.Message := ROSIDL.Dynamic.Init (Support);
+   MsgRot  : ROSIDL.Dynamic.Message := ROSIDL.Dynamic.Init (Support);
 
    Node : Nodes.Node := Nodes.Init   (Utils.Command_Name);
 
@@ -28,13 +30,31 @@ begin
       return;
    elsif Argument_Count = 1 then
       Put_Line ("No command given, using default pattern...");
+      MsgLin ("linear.x").As_Float64  := 3.0;
+      MsgRot ("angular.z").As_Float64 := 2.0;
    else
       null;
    end if;
 
    declare
-      Pub  : Publishers.Publisher := Node.Publish (Support, Argument (1));
+
+      Pub    : Publishers.Publisher := Node.Publish (Support, Argument (1));
+      Next   : Time    := Clock;
+      Rotate : Boolean := False;
    begin
-      null;
+      loop
+         if Next < Clock then
+            if Rotate then
+               Pub.Publish (MsgRot);
+            else
+               Pub.Publish (MsgLin);
+            end if;
+
+            Rotate := not Rotate;
+
+            Next := Clock + 1.0;
+            Logging.Info ("Sending...");
+         end if;
+      end loop;
    end;
 end Turtlesim_Command;
